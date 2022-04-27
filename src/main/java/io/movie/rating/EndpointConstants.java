7832cbae0c448677e6cb1bae16630fcb81442655
Jānis Vīksne <viksnejanis7@gmail.com>
2022-04-27 17:04:45 +0200

    CLEANUP CREW

--- src/main/java/io/movie/rating/EndpointConstants.java
+++ src/main/java/io/movie/rating/EndpointConstants.java
@@ -1,13 +1,10 @@
 package io.movie.rating;
 
-public final class EndpointConstants {
+  class EndpointConstants {
 
     public static final String SHOW_ALL_MOVIES = "show-all-movies";
     public static final String REDIRECT_SHOW_ALL_MOVIES = "redirect:/show-all-movies";
     public static final String ADD_NEW_MOVIE = "add-new-movie";
     public static final String MOVIE = "movie";
 
-    public EndpointConstants() {
-
-    }
 }
